5f5f26a0c285b4afa8d59ccacaba6fe1e690756d
Adrian <seraphzp@gmail.com>
2026-01-20 19:14:41 +0900

    update

--- Source/Aura/Public/Game/AuraGameModeBase.h
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -6,6 +6,7 @@
 #include "GameFramework/GameModeBase.h"
 #include "AuraGameModeBase.generated.h"
 
+class UAuraUserWidget;
 /**
  * 
  */
@@ -13,5 +14,4 @@ UCLASS()
 class AURA_API AAuraGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
 };
