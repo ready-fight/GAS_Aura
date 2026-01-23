6db1350c4dffaa0e22f88f74aa47ad86f8784921
Adrian <seraphzp@gmail.com>
2026-01-23 19:00:48 +0900

    added BP_OverlayWidgetController

---
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -0,0 +1,13 @@
+
+
+
+#include "UI/WidgetController/OverlayWidgetController.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
+void UOverlayWidgetController::BroadcastInitialValues()
+{
+	UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+	OnHealthChanged.Broadcast(AuraAttributeSet->GetHealth());
+	OnMaxHealthChanged.Broadcast(AuraAttributeSet->GetMaxHealth());
+}
